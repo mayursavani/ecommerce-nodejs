391738659eb0c53c140ad882098ffdafdfc5fb01
mayursvani <mayursavani99@gmail.com>
2023-05-04 11:31:11 +0530

    added middlewares for the app

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,21 @@
+const config = require("../config/config");
+const jwt = require("jsonwebtoken");
+
+const isAuth = (req, res, next) => {
+  let token = req.get("Authorization");
+
+  jwt.verify(token, config.SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: "Token Invalid",
+      });
+    }
+    req.user = user.data;
+    next();
+  });
+};
+
+module.exports = {
+  isAuth,
+};
